d809f428a2904a770f814a272f366d3024a467fd
voromahery <daniel.her@onja.org>
2025-06-11 13:54:40 +0300

    Started challenges

--- challenges/two-sum/index.js
+++ challenges/two-sum/index.js
@@ -1,6 +1,12 @@
 function twoSum(nums, target) {
     // Write your code here
-    console.log("test");
+    for (let i = 0; i < nums.length; i++) {
+      for (let j = 1; j < nums.length; j++) {
+        if (nums[i] + nums[j] === target) {
+          return [i, j];
+        }
+      }
+    }
 }
 
  function runTests() {
