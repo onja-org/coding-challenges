64dbb6d4a4d6eae8ce42864b4bfe9a076873141e
Arisoa <sandy.rav@onja.org>
2025-06-11 13:58:18 +0300

    solve two-sum coding challenge

--- challenges/two-sum/index.js
+++ challenges/two-sum/index.js
@@ -1,37 +1,46 @@
 function twoSum(nums, target) {
-    // Write your code here
-    console.log("test");
-}
-
- function runTests() {
-    const testCases = [
-      {
-        nums: [2, 7, 11, 15],
-        target: 9,
-        expected: [0, 1]
-      },
-      {
-        nums: [3, 2, 4],
-        target: 6,
-        expected: [1, 2]
-      },
-      {
-        nums: [3, 3],
-        target: 6,
-        expected: [0, 1]
+  // Write your code here
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
       }
-    ];
-
-    testCases.forEach(({ nums, target, expected }, index) => {
-      const result = twoSum(nums, target);
-      const pass =
-        (result[0] === expected[0] && result[1] === expected[1]) ||
-        (result[0] === expected[1] && result[1] === expected[0]);
-
-      console.log(`Test Case ${index + 1}:`, pass ? '✅ Passed' : '❌ Failed');
-      console.log(`Input: nums = [${nums}], target = ${target}`);
-      console.log(`Expected: [${expected}], Got: [${result}]\n`);
-    });
+    }
   }
 
-  runTests();
+  console.log("test");
+  return [];
+}
+
+function runTests() {
+  const testCases = [
+    {
+      nums: [2, 7, 11, 15],
+      target: 9,
+      expected: [0, 1],
+    },
+    {
+      nums: [3, 2, 4],
+      target: 6,
+      expected: [1, 2],
+    },
+    {
+      nums: [3, 3],
+      target: 6,
+      expected: [0, 1],
+    },
+  ];
+
+  testCases.forEach(({ nums, target, expected }, index) => {
+    const result = twoSum(nums, target);
+    const pass =
+      (result[0] === expected[0] && result[1] === expected[1]) ||
+      (result[0] === expected[1] && result[1] === expected[0]);
+
+    console.log(`Test Case ${index + 1}:`, pass ? "✅ Passed" : "❌ Failed");
+    console.log(`Input: nums = [${nums}], target = ${target}`);
+    console.log(`Expected: [${expected}], Got: [${result}]\n`);
+  });
+}
+
+runTests();
