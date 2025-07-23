24904966b349b7c2f3a6fab10f9d704682325e4b
starjardin <tantely.and@onja.org>
2025-07-23 09:00:24 +0300

    [challenge] Adds is palyndrome challenge

--- challenges/isPalindrome/index.js
+++ challenges/isPalindrome/index.js
@@ -1,7 +1,35 @@
-function isPalindrome(s) {
- const cleaned = s.toLowerCase().replace(/[^a-z0-9]/g, '');
-    return cleaned === cleaned.split('').reverse().join('');
+// function isPalindrome(s) {
+//  const cleaned = s.toLowerCase().replace(/[^a-z0-9]/g, '');
+//     return cleaned === cleaned.split('').reverse().join('');
+// }
+
+function isAlphanumeric(char) {
+    return /^[a-z0-9]$/i.test(char);
 }
+
+
+function isPalindrome(s) {
+    let left = 0;
+    let right = s.length -1;
+
+    while (left < right) {
+        while (left < right && !isAlphanumeric(s[left])) {
+            left++;
+        }
+        while (left < right && !isAlphanumeric(s[right])) {
+            right--;
+        }
+
+        if (s[left].toLowerCase() !== s[right].toLowerCase()) {
+            return false;
+        }
+
+        left++;
+        right--;
+    }
+    return true;
+}
+
 
 // Essential palindrome tests with additional edge cases
 function testPalindrome(isPalindrome) {
